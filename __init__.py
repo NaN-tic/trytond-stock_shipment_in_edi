9ad8a3559f7579aeaf944cae9be457c6a2f4a28a
Bernat <bernat@nan-tic.com>
2021-02-11 12:34:51 +0100

    Merge pull request #1 from NaN-tic/042121
    042121

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from . import shipment
-
+from . import edi_shipment
 
 def register():
     Pool.register(
-        shipment.Cron,
-        shipment.Move,
-        shipment.ShipmentIn,
-        shipment.StockConfiguration,
+        edi_shipment.Cron,
+        edi_shipment.SupplierEdi,
+        edi_shipment.EdiShipmentReference,
+        edi_shipment.EdiShipmentInLine,
+        edi_shipment.EdiShipmentIn,
+        edi_shipment.EdiShipmentInLineQty,
+        edi_shipment.StockConfiguration,
         module='stock_shipment_in_edi', type_='model')
